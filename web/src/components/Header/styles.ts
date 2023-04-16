e1cd62ab721659cc3e404a7796806babc964d89d
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-04-15 22:21:40 -0300

    Feature: Component Header

---
+++ web/src/components/Header/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  div {
+    display: flex;
+    align-items: center;
+    font-size: 1rem;
+
+    gap: 1rem;
+  }
+`;
